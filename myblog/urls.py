bcb7eb4362982405ab56388a38fdfb2ce177c55d
Abdelhakim Aouay <hkim_away@yahoo.fr>
2023-04-23 15:03:22 +0200

    ajout les classe Listeview et detailView avec URL et templates

--- myblog/urls.py
+++ myblog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     
-    path('home/', views.home),
+    path('home/', views.HomeListView.as_view(), name="home"),
+    path('article/<int:pk>/', views.ArticleDetailView.as_view(), name="article"),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-
+from.models import Post
+from django.views.generic import ListView, DeleteView, DetailView
 # Create your views here.
 def home(request):
     return render(request, 'myblog/home.html', {})
+
+class HomeListView(ListView):
+    model=Post
+    template_name="myblog/home.html"
+    
+    
+class ArticleDetailView(DetailView):
+    model=Post
+    template_name="myblog/article.html"
+    
